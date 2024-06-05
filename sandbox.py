37a448f48587fc416f8cdbda1b138c77d3e995a8
Alex Powell <alexpow@gmail.com>
2024-06-05 17:18:01 -0400

    Delete sandbox.py

--- sandbox.py
+++
@@ -1,6 +0,0 @@
-from integration.products import *
-
-if __name__ == '__main__':
-    integrator = Integrator(date_presets.business_start_date)
-    customers = integrator.Customers(last_sync=date_presets.business_start_date)
-    customers.sync()
