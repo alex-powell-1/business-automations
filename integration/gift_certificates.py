27e3e9fd39ce756b586c57f766bffdf6e0fe07f5
Brandon Settlemyre <sales@Lenovo-Flex5.SETTLEMYRE.local>
2024-06-06 14:23:45 -0400

    Gift Certificates Start

--- integration/gift_certificates.py
+++ integration/gift_certificates.py
@@ -1,2 +1,27 @@
+import requests
+
+import integration.utilities as utilities
+from integration.database import Database
+from setup import creds
+
 class GiftCertificates:
-    pass
+    def __init__(self, last_sync):
+        self.last_sync = last_sync
+        self.db = Database.db
+
+    def get_certificates(self):
+        query = f"""
+        SELECT CUST_NO, FST_NAM, LST_NAM, EMAIL_ADRS_1, PHONE_1, LOY_PTS_BAL, ADRS_1, CITY, STATE, ZIP_COD, CNTRY
+        FROM AR_CUST
+        WHERE
+        LST_MAINT_DT > '{self.last_sync}' and
+        CUST_NAM_TYP = 'P'
+        """
+        
+        response = self.db.query_db(query)
+        if response is not None:
+            result = []
+            for x in response:
+                if x is not None:
+                    result.append(self.Customer(x))
+            return result
