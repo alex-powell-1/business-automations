0c46362a16cb022d77d8d00551d2f9f3bfe28c85
Alex <alex@settlemyrenursery.com>
2024-06-12 11:14:18 -0400

    Update delete variants

--- orders/orders_tools.py
+++ orders/orders_tools.py
@@ -67,8 +67,6 @@ def post_document():
                     "LIN_TYP": "S",
                     "ITEM_NO": "BTSP4MP",
                     "QTY_SOLD": "2",
-                    "DISC_COD": "WEBLINE",
-                    "DISC_AMT": "2.00",
                 }
             ],
             "PS_DOC_HDR_MISC_CHRG": [
@@ -77,6 +75,13 @@ def post_document():
             "PS_DOC_PMT": [
                 {"AMT": 63.98, "PAY_COD": "BIG", "FINAL_PMT": "N"},
                 {"AMT": 10, "PAY_COD": "LOYALTY", "FINAL_PMT": "N"},
+                {
+                    "PAY_COD": "GC",
+                    "AMT": 10,
+                    "FINAL_PMT": "N",
+                    "PMT_LIN_TYP": "T",
+                    "CARD_NO": "1234567890",
+                },
             ],
             "PS_DOC_TAX": [
                 {
@@ -85,31 +90,75 @@ def post_document():
                     "TAX_DOC_PART": "S",
                     "TAX_AMT": "0",
                     "TOT_TXBL_AMT": 11.99,  # not shipping
-                }
+                },
             ],
-            # "PS_DOC_DISC": [
-            #     {
-            #         "DISC_COD": "WEBLINE",
-            #         "LIN_SEQ_NO": 1,
-            #         "DISC_AMT": 2.00,
-            #     }
-            # ],
+            "PS_DOC_DISC": [
+                {"LIN_SEQ_NO": 1, "DISC_ID": "100000000000101", "DISC_AMT": 5}
+            ],
         },
-        # "PS_DOC_HDR_TOT": [
-        #     {
-        #         # "LINS": 1,
-        #         "SUB_TOT": 11.99,
-        #         "TOT_MISC": 50,
-        #         "TOT": 61.99,
-        #         "TOT_TND": 61.99,
-        #         "AMT_DUE": 0,
-        #         # "TOT_HDR_DISCNTBL_AMT": 11.99
-        #     }
-        # ],
         "PS_TAX": {"ORD_NORM_TAX_AMT": 0, "ORD_TAX_AMT": 0},
     }
 
-    response = requests.post(url, headers=headers, json=payload, verify=False)
+    refund_payload = {
+        "PS_DOC_HDR": {
+            "STR_ID": "WEB",
+            "STA_ID": "WEB",
+            "DRW_ID": "1",
+            "CUST_NO": "105786",
+            "LOY_PGM_COD": "BASIC",
+            "TKT_TYP": "T",
+            "DOC_TYP": "T",
+            "USR_ID": "POS",
+            "TAX_COD": "EXEMPT",
+            "NORM_TAX_COD": "EXEMPT",
+            "SHIP_VIA_COD": "CPC_FLAT",
+            "TAX_OVRD_REAS": "Y",
+            "HAS_ENTD_LINS": "N",
+            "PS_DOC_NOTE": [{"NOTE_ID": "NOTE1", "NOTE": "eCommerce order test #2"}],
+            "PS_DOC_LIN": [
+                {
+                    "LIN_TYP": "R",
+                    "ITEM_NO": "BTSP4MP",
+                    "QTY_SOLD": 2,
+                    "PRC": 11.99,
+                }
+            ],
+            "PS_DOC_HDR_MISC_CHRG": [
+                {
+                    "TOT_TYP": "S",
+                    "MISC_CHRG_NO": "1",
+                    "MISC_TYP": "A",
+                    "MISC_AMT": -50.00,
+                }
+            ],
+            "PS_DOC_PMT": [
+                {
+                    "AMT": 73.98,
+                    "PAY_COD": "BIG",
+                    "FINAL_PMT": "N",
+                    "PMT_LIN_TYP": "C",
+                    "DESCR": "Big Commerce",
+                },
+            ],
+            # "PS_DOC_PMT_APPLY": [
+            #     {
+            #         "AMT": -73.98,
+            #         "APPL_TYP": "S",
+            #     },
+            # ],
+            "PS_DOC_TAX": [
+                {
+                    "AUTH_COD": "EXEMPT",
+                    "RUL_COD": "TAX",
+                    "TAX_DOC_PART": "S",
+                    "TAX_AMT": "0",
+                    "TOT_TXBL_AMT": -23.98,  # not shipping
+                }
+            ],
+        },
+    }
+
+    response = requests.post(url, headers=headers, json=refund_payload, verify=False)
     pretty = response.content
     pretty = json.loads(pretty)
     pretty = json.dumps(pretty, indent=4)
