c17bf851b3044dada1b7c792f12c03279c38e99c
Luke Barrier <lukebbarrier06@outlook.com>
2024-09-17 11:03:26 -0400

    Update

--- product_tools/sort_order.py
+++ product_tools/sort_order.py
@@ -74,8 +74,9 @@ class SortOrderEngine:
             for item_index, item in enumerate(items):
 
                 def insert_item_at(index1, index2):
+                    item = items[index1]
                     items.pop(index1)
-                    items.insert(index2, items[index1])
+                    items.insert(index2, item)
 
                 # print('ITEM: ', item['item_no'])
                 if item['price_2'] is not None and item['price_1'] > item['price_2']:
