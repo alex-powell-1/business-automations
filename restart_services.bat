adcfda6981c147e6c1d590998d04bdf9676d9ed3
Alex Powell <alexpow@gmail.com>
2024-09-23 06:00:42 -0400

    add restart services

---
+++ restart_services.bat
@@ -0,0 +1,11 @@
+sc stop SN-Server
+sc stop SN-ngrok
+sc stop SN-Tasks
+sc stop SN-Integrator
+sc stop SN-Inventory
+TIMEOUT /t 3
+sc start SN-ngrok
+sc start SN-Server
+sc start SN-Tasks
+sc start SN-Integrator
+sc start SN-Inventory
