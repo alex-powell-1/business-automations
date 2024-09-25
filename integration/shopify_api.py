f0121af64178e41e16a8493611254fcb8d56d8d2
Alex Powell <alexpow@gmail.com>
2024-09-25 13:13:31 -0400

    Update

--- integration/shopify_api.py
+++ integration/shopify_api.py
@@ -72,7 +72,7 @@ class Shopify:
 
     class Query:
         def __init__(self, document, variables=None, operation_name=None):
-            self.verbose = True
+            self.verbose = False
             self.response = self.execute_query(document, variables, operation_name)
             self.data = self.response['data'] if 'data' in self.response else None
             self.errors: list = self.response['errors'] if 'errors' in self.response else []
